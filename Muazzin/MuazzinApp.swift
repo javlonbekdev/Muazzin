f903140d0a8bffa0e73eb9b0787f8dced2f01b1f
Javlonbek <javlonbekdev@gmail.com>
2023-01-29 10:41:45 +0500

    Initial Commit

---
+++ Muazzin/MuazzinApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuazzinApp.swift
+//  Muazzin
+//
+//  Created by Javlonbek on 29/01/23.
+//
+
+import SwiftUI
+
+@main
+struct MuazzinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
